06710fe80f5331b69f43da815d2e47293911abda
pbain63 <prodipbain63@gmail.com>
2024-03-15 05:52:58 +0600

    Add text to the container and add the container to the body by DOM in script.js

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ const container = document.createElement("div");
 
 container.setAttribute("id", "container");
 
+container.textContent = "THis is testing";
+
+
+
+document.body.appendChild(container);
+
+
