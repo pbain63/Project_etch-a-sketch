1ab7d6e1bf89dda9a61d0000c42a1c61c5a997c2
pbain63 <prodipbain63@gmail.com>
2024-03-16 21:19:08 +0600

    'Modify script.js'
    "Select all grid by querySelectorAll() to iterate mouseMoves"

--- script.js
+++ script.js
@@ -2,19 +2,28 @@ const container = document.createElement("div");
 
 container.setAttribute("id", "container");
 
-// container.textContent = "THis is testing"; 
-
-
 document.body.appendChild(container);
-
 
 let htmlElements = "";
 
 for (let i = 0; i < 256; i++) {
-    htmlElements += "<div class = 'grid'></div>";
-    
-    
+  htmlElements += '<div class = "grid"></div>';
 }
 
 container.innerHTML = htmlElements;
-// console.log(htmlElements);
+
+// -
+
+const mouseMoves = document.querySelectorAll(".grid");
+
+// mouseMoves.forEach((mouseMove) => {
+//   mouseMove.addEventListener("mouseenter", () => {
+//     //("mouseenter", sketch)
+//     mouseMove.style.backgroundColor = "orange";
+//   });
+// });
+
+// function sketch(e) {
+//   // e.target.style.backgroundColor = "orange";  //
+// }
+// -
