05f449a3dbdc62fef635c21e0a8f5cda331bbaa5
pbain63 <prodipbain63@gmail.com>
2024-03-17 07:52:38 +0600

    'Modify script.js'
    "Add querySelector to the button id, so we can identify
    button presses"

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
+let gridChangeBtn = document.querySelector("#gridChangeBtn");
+
 const container = document.createElement("div");
 
 container.setAttribute("id", "container");
 
 document.body.appendChild(container);
 
-let htmlElements = "";
+// gridChangeBtn.addEventListener("click", changeGrid);
 
-for (let i = 0; i < 256; i++) {
-  htmlElements += '<div class = "grid"></div>';
-}
+// function changeGrid(e) {
+//   let userInputHeight = Number(prompt("Enter a preferred height for grid: "));
+//   let userInputWidth = Number(prompt("Enter a preferred width for grid: "));
+//   let gridShape = userInputHeight * userInputWidth;
 
-container.innerHTML = htmlElements;
+//   let htmlElements = "";
 
-// -
+//   for (let i = 0; i < gridShape; i++) {
+//     htmlElements += '<div class = "grid"></div>';
+//   }
 
-const mouseMoves = document.querySelectorAll(".grid");
+//   container.innerHTML = htmlElements;
+//   // console.log(container.innerHTML);
+//   //
+//   const mouseMoves = document.querySelectorAll(".grid");
 
-mouseMoves.forEach((mouseMove) => {
-  mouseMove.addEventListener("mouseenter", () => {
-    //("mouseenter", sketch)
-    mouseMove.style.backgroundColor = "orange";
-  });
-});
+//   mouseMoves.forEach((mouseMove) => {
+//     mouseMove.addEventListener("mouseenter", () => {
+//       //("mouseenter", sketch)
+//       mouseMove.style.backgroundColor = "orange";
+//     });
+//   });
+//   // function sketch(e) {
+//   //   // e.target.style.backgroundColor = "orange";  //
+//   // }
 
-// function sketch(e) {
-//   // e.target.style.backgroundColor = "orange";  //
+  
 // }
-// -
+
+
+// // -
