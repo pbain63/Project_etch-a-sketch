67885c155a62d2238f788ee7fa8753ce07eb3078
pbain63 <prodipbain63@gmail.com>
2024-03-19 08:33:49 +0600

    'Modify script.js'
    "In the changeGrid function remove first child to add new first
    child with while loop, get gridWidth, gridHeight by dividing containerSize
    with the userGridSize. Use another for loop to get gridShape and
     put gridWidth/height to style the grid"

--- script.js
+++ script.js
@@ -23,43 +23,43 @@ for (let i = 0; i < 256; i++) {
   container.appendChild(grid);
 }
 
-// gridChangeBtn.addEventListener("click", changeGrid);
+gridChangeBtn.addEventListener("click", changeGrid);
 
-// function changeGrid(e) {
-//   let userGridSize = Number(
-//     prompt("Enter a preferred size for grid up to 100: ")
-//   );
-//   if (userGridSize > 100) {
-//     alert("Please enter grid size between 100!");
-//   } else {
-//     let gridShape = userGridSize * userGridSize;
+function changeGrid(e) {
+  let userGridSize = Number(
+    prompt("Enter a preferred size for grid up to 100: ")
+  );
+  if (userGridSize > 100) {
+    alert("Please enter grid size between 100!");
+  } else {
+    let gridShape = userGridSize * userGridSize;
 
-//     let containerSize = Number(500);
-//     let gridWidth = 500 / userGridSize;
-//     let gridHeight = 500 / userGridSize;
+    let containerSize = Number(500);
+    let gridWidth = 500 / userGridSize;
+    let gridHeight = 500 / userGridSize;
 
-//     while (container.firstChild) {
-//       container.removeChild(container.firstChild);
-//     }
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
 
-//     for (let i = 0; i < gridShape; i++) {
-//       const grid = document.createElement("div");
-//       grid.classList.add("gridBox");
+    for (let i = 0; i < gridShape; i++) {
+      const grid = document.createElement("div");
+      grid.classList.add("gridBox");
 
-//       grid.style.width = gridWidth + "px";
-//       grid.style.height = gridHeight + "px";
-//       console.log(grid.style.height);
+      grid.style.width = gridWidth + "px";
+      grid.style.height = gridHeight + "px";
+      console.log(grid.style.height);
 
-//       container.appendChild(grid);
-//     }
+      container.appendChild(grid);
+    }
 
-//     const mouseMoves = document.querySelectorAll(".gridBox");
+    const mouseMoves = document.querySelectorAll(".gridBox");
 
-//     mouseMoves.forEach((mouseMove) => {
-//       mouseMove.addEventListener("dragover", sketch); //dragover//click//mousedown
-//     });
-//     function sketch(e) {
-//       e.target.style.backgroundColor = "orange";
-//     }
-//   }
-// }
+    mouseMoves.forEach((mouseMove) => {
+      mouseMove.addEventListener("dragover", sketch); //dragover//click//mousedown
+    });
+    function sketch(e) {
+      e.target.style.backgroundColor = "orange";
+    }
+  }
+}
