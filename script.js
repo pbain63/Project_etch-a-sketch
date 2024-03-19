1da193ee3586f949f31093e5bc8b1a6d307ffb90
pbain63 <prodipbain63@gmail.com>
2024-03-19 08:19:17 +0600

    Add width, height to container in script.js

--- script.js
+++ script.js
@@ -6,13 +6,16 @@ const container = document.createElement("div");
 container.setAttribute("id", "container");
 mainContainer.setAttribute("id", "mainContainer");
 
-// let containerWidth = (container.style.width = "500px");
-// let containerHeight = (container.style.height = "500px");
+let containerWidth = (container.style.width = "500px");
+let containerHeight = (container.style.height = "500px");
 
 // document.body.appendChild(mainContainer);
 
 // mainContainer.appendChild(container);
-
+// // let gridReset = document.querySelector("#gridReset");
+// // gridReset.addEventListener("click", changeGrid);
+// // function drawGrid() {
+// // }
 // for (let i = 0; i < 256; i++) {
 //   const grid = document.createElement("div");
 //   grid.classList.add("grid");
@@ -23,27 +26,27 @@ mainContainer.setAttribute("id", "mainContainer");
 // gridChangeBtn.addEventListener("click", changeGrid);
 
 // function changeGrid(e) {
-//   let userGridSize = Number(prompt("Enter a preferred size for grid up to 100: "));
+//   let userGridSize = Number(
+//     prompt("Enter a preferred size for grid up to 100: ")
+//   );
 //   if (userGridSize > 100) {
 //     alert("Please enter grid size between 100!");
 //   } else {
 //     let gridShape = userGridSize * userGridSize;
 
 //     let containerSize = Number(500);
-//     let gridWidth = 500 / userGridSize; /////////
+//     let gridWidth = 500 / userGridSize;
 //     let gridHeight = 500 / userGridSize;
 
-//     // console.log(gridWidth);
-
 //     while (container.firstChild) {
-//       container.removeChild(container.firstChild); //
+//       container.removeChild(container.firstChild);
 //     }
 
 //     for (let i = 0; i < gridShape; i++) {
 //       const grid = document.createElement("div");
 //       grid.classList.add("gridBox");
 
-//       grid.style.width = gridWidth + "px"; /////////////
+//       grid.style.width = gridWidth + "px";
 //       grid.style.height = gridHeight + "px";
 //       console.log(grid.style.height);
 
@@ -53,13 +56,10 @@ mainContainer.setAttribute("id", "mainContainer");
 //     const mouseMoves = document.querySelectorAll(".gridBox");
 
 //     mouseMoves.forEach((mouseMove) => {
-//       mouseMove.addEventListener("mouseenter", () => {
-//         //("mouseenter", sketch)
-//         mouseMove.style.backgroundColor = "orange";
-//       });
+//       mouseMove.addEventListener("dragover", sketch); //dragover//click//mousedown
 //     });
-//     //   // function sketch(e) {
-//     //   //   // e.target.style.backgroundColor = "orange";  //
-//     //   // }
+//     function sketch(e) {
+//       e.target.style.backgroundColor = "orange";
+//     }
 //   }
 // }
